7df223b572be7737470476dd5a651948d680bb7d
Hopman <3tonchik@mail.ru>
2020-11-07 15:19:26 +0300

    module5-task1

---
+++ db/create-db.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE "typoteka-153671"
+    WITH
+    OWNER = postgres
+    ENCODING = 'UTF-8'
+    LC_COLLATE = 'C'
+    LC_CTYPE = 'C'
+    TABLESPACE = pg_default
+    TEMPLATE template0
+    CONNECTION LIMIT = -1;
